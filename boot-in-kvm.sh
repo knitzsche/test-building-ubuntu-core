d61e1c764890d2e3a2c57e32b9abcfc94e3fc7e9
dbruno74 <65651317+dbruno74@users.noreply.github.com>
2020-12-09 21:45:04 +0100

    Updated uc20 kvm parameters

--- boot-in-kvm.sh
+++ boot-in-kvm.sh
@@ -11,4 +11,8 @@ fi
 
 IMG=$1
 
-kvm -smp 2 -m 1500 -netdev user,id=mynet0,hostfwd=tcp::8022-:22,hostfwd=tcp::8090-:80 -device virtio-net-pci,netdev=mynet0 -vga qxl -drive file=$IMG,format=raw
+sudo qemu-system-x86_64 -smp 1 -m 2048 \
+ -net nic,model=virtio -net user,hostfwd=tcp::8022-:22 \
+ -drive file=/usr/share/OVMF/OVMF_CODE.fd,if=pflash,format=raw,unit=0,readonly=on \
+ -drive file=$IMG,cache=none,format=raw,id=disk1,if=none \
+ -device virtio-blk-pci,drive=disk1,bootindex=1 -machine accel=kvm
